9fe9e76a0283b6925cd21ea167a7446ea6067f40
Matthew Chan <matt@themattchan.com>
2016-04-09 21:38:41 -0700

    extend

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,8 @@ instance Extend Id where
     (Id a -> b)
     -> Id a
     -> Id b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Id"
+  f <<= a = Id $ f a
+
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +51,9 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  f <<= Nil        = Nil
+  f <<= xs@(_:.ts) = f xs :. (f <<= ts)
+
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +67,9 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  _ <<= Empty = Empty
+  f <<= op    = Full $ f op
+
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +88,4 @@ cojoin ::
   Extend f =>
   f a
   -> f (f a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (id <<=)
